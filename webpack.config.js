1650d8e4226201829d286de695c85c47ebeed593
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-04 17:09:40 -0600

    Setup latest webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const SRC_DIR = path.resolve(__dirname, './client/src');
+const DIST_DIR = path.resolve(__dirname, './client/dist');
+
+module.exports = {
+  mode: "production",
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: SRC_DIR,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+};
