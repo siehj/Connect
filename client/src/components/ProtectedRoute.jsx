2582f04d9d3811d8e67b7dbe50eccab8d930e92d
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-04 19:38:47 -0600

    Created a loading screen with interactive loading display and css effects for user experience

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -6,21 +6,34 @@ class ProtectedRoute extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      loading: true
+      loading: true,
+      loadingCount: 10
     };
   }
 
   componentDidMount() {
-
+ 
   }
+
+
 
   render() {
     // return <Route render={() => auth.authCheck() ? <props.component /> : <Redirect to="/login" /> } />
-    // this.state.loading === true ? 
+    // this.state.loading === true ?
+ 
+    this.state.loadingCount <= screen.width ?
+    setTimeout(() => {
+      this.state.loadingCount <= screen.width ? this.setState({ loadingCount : this.state.loadingCount+(screen.width/100) }, () => console.log(this.state.loadingCount)) : this.setState({ loading: false }, () => console.log('done loading!'))
+    }, 100) : console.log(this.state.loadingCount <= screen.width);
+
     return (
       <div className="loading" >
+        <div className="loadingOverlay" style={{ width: `${this.state.loadingCount}px` }} >
+          {
+            this.state.loadingCount >= screen.width ? <h1 className="bounce-top">WELCOME</h1> : null
+          }
+        </div> 
         <h1 > <a>...</a> LOADING<a>...</a></h1>
-        <div className="loadingOverlay" ></div>
       </div>
     ) 
     // : null;
