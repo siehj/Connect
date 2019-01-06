5d47ad2a9f6be7b88bf8769a5251cff640b0bf82
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-05 23:17:22 -0600

    Set up a protected route with a loading screen

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -11,33 +11,33 @@ class ProtectedRoute extends React.Component {
     };
   }
 
-  componentDidMount() {
- 
-  }
-
-
-
+  
   render() {
     // return <Route render={() => auth.authCheck() ? <props.component /> : <Redirect to="/login" /> } />
-    // this.state.loading === true ?
- 
-    this.state.loadingCount <= screen.width ?
-    setTimeout(() => {
-      this.state.loadingCount <= screen.width ? this.setState({ loadingCount : this.state.loadingCount+(screen.width/100) }, () => console.log(this.state.loadingCount)) : this.setState({ loading: false }, () => console.log('done loading!'))
-    }, 100) : console.log(this.state.loadingCount <= screen.width);
-
-    return (
-      <div className="loading" >
-        <div className="loadingOverlay" style={{ width: `${this.state.loadingCount}px` }} >
-          {
-            this.state.loadingCount >= screen.width ? <h1 className="bounce-top">WELCOME</h1> : null
-          }
-        </div> 
-        <h1 > <a>...</a> LOADING<a>...</a></h1>
-      </div>
-    ) 
-    // : null;
+  //   this.state.loadingCount <= screen.width ?
+  //   setTimeout(() => {
+  //     this.state.loadingCount <= screen.width ? this.setState({ loadingCount : this.state.loadingCount+(screen.width/80) }) : this.setState({ loading: false })
+  //   }, 50) : setTimeout(() => this.setState({ loading: false }), 2500);
+    
+    
+  //   if (this.state.loading === true) {
+  //     return (
+  //       <div className="loading" >
+  //         <div className="loadingOverlay" style={{ width: `${this.state.loadingCount}px` }} >
+  //           {
+  //             this.state.loadingCount >= screen.width ? <h1 className="bounce-top">WELCOME</h1> : null
+  //           }
+  //         </div> 
+  //         <h1>...LOADING...</h1>
+  //       </div>
+  //     ) 
+  //   } 
+  // if (this.state.loading === false) return <Route render={() => <this.props.component />} />
+    
+  //   // : null;
+    return <this.props.component />
   }
 }
+
 
 export default ProtectedRoute;
