c6de2eadb00d7e4be19ae8470bdf196ba36374a8
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-07 17:30:17 -0600

    Create user connections component

---
+++ client/src/components/nestedComponents/DashboardComponents/Connections.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Connections extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Connections</h1>
+      </div>
+    )
+  }
+}
+
+export default Connections;
