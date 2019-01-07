30ea23e568870feb5eea814d0cc2b9dc64b03414
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-07 17:29:47 -0600

    Implement Chart component

---
+++ client/src/components/nestedComponents/DashboardComponents/Charts.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Charts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Charts</h1>
+      </div>
+    )
+  }
+}
+
+export default Charts;
