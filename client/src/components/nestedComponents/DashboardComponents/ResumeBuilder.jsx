4da58061968ad2efd5122ca3f5c0281fe4c2751e
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-07 17:30:39 -0600

    Create user resume builder component

---
+++ client/src/components/nestedComponents/DashboardComponents/ResumeBuilder.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ResumeBuilder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Resume Builder</h1>
+      </div>
+    )
+  }
+}
+
+export default ResumeBuilder;
