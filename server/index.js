2ccd9388be765cc7caf08d2688b18c1aadde59d1
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-10 20:33:16 -0600

    Work on routing between pre-app access components

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+var bodyParser = require('body-parser');
+const router = require('./routes/index.js');
 
 let app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`)
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,8 @@
-const router = express.Router();
+const router = require('express').Router();
+const path = require('path');
 
+router.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), (err) => err ? res.redirect(`/`): null)
+});
 
 module.exports = router;
