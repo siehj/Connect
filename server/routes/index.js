c428cb07f51d0817a1e58b75f2d3e843f86d37df
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-11 00:55:45 -0600

    Make minor change to the routes to deploy app

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const path = require('path');
 
-router.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'), (err) => err ? res.redirect(`/`): null)
-});
+// router.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/dist/index.html'), (err) => err ? res.redirect(`/`): null)
+// });
 
 router.post('/api/login', (req, res) => {
   console.log(req.body);
